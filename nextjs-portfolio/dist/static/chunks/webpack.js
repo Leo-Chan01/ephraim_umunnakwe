ea713a4821c2bffc5b536c51b5fdef4edfac9b46
Leo-Chan01 <54512508+Leo-Chan01@users.noreply.github.com>
2026-01-12 15:36:34 -0800

    chore: update baseline-browser-mapping to version 2.9.14 in package.json and package-lock.json

--- nextjs-portfolio/dist/static/chunks/webpack.js
+++ nextjs-portfolio/dist/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8692df0ebb62e9e8"; }
+/******/ 		__webpack_require__.h = function() { return "0754be32b997d158"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
